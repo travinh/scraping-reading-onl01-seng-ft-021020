c2091bd7458cdf4a3420f4878642c11b58490f78
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-02 07:17:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,4 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-name= doc.css(".headline_260IBN").text
-name
+doc.css(".headline_260IBN").text
